6b8e846faf9b04a459f48ebe80c51992c360fad0
knilkreb <berkling@yahoo.com>
2014-05-14 10:39:17 +0200

    extract and move method getFrequentRenterPoints to Rental

--- src/Customer.java
+++ src/Customer.java
@@ -26,11 +26,8 @@ class Customer {
             Rental each = (Rental) enum_rentals.nextElement();
             //determine amounts for each line
             thisAmount = each.getCharge();
-            // add frequent renter points
-            frequentRenterPoints ++;
-            // add bonus for a two day new release rental
-            if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1) 
-                frequentRenterPoints ++;
+            frequentRenterPoints += each.getFrequentRenterPoints();
+
             //show figures for this rental
             result += "\t" + each.getMovie().getTitle()+ "\t" + "\t" + each.getDaysRented() + "\t" + String.valueOf(thisAmount) + "\n";
             totalAmount += thisAmount;
--- src/Rental.java
+++ src/Rental.java
@@ -31,4 +31,13 @@ class Rental {
         }
         return result;
     }
+
+    public int getFrequentRenterPoints() {
+        // add frequent renter points
+        // add bonus for a two day new release rental
+        if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) && getDaysRented() > 1)
+            return 2;
+        return 1;
+    }
+
 }
