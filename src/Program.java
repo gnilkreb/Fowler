8a4ad646ef423b279a2290526c16cd2a24c25f45
Rico <r.jambor@yahoo.de>
2017-05-09 12:07:45 +0200

    Remove redundand variable declaration

--- src/Program.java
+++ src/Program.java
@@ -14,7 +14,6 @@ public class Program
     
     public static void main(String args[]) 
     {
-        String result;
         System.out.println("Welcome to the Movie Store");
         Movie m1 = new Movie("movie1", 1);
         Movie m2 = new Movie("movie2", 2);
@@ -23,7 +22,7 @@ public class Program
         Customer c1 = new Customer("joe");
         c1.addRental(r1);   c1.addRental(r2);   
         System.out.println("Let's get the Statement");
-        result = c1.statement();
+        String result = c1.statement();
         System.out.println(result);
     }
 }
