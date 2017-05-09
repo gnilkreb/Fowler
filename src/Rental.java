94d8ca930c8fcb4142b7a79f6633d355e27fddf4
Rico <r.jambor@yahoo.de>
2017-05-09 12:06:51 +0200

    Refactor variable names in Rental constructor

--- src/Rental.java
+++ src/Rental.java
@@ -1,9 +1,10 @@
 class Rental {
     private Movie movie;
     private int daysRented;
-    public Rental(Movie newmovie, int newdaysRented) {
-        movie = newmovie;
-        daysRented = newdaysRented;
+
+    public Rental(Movie movie, int daysRented) {
+        this.movie = movie;
+        this.daysRented = daysRented;
     }
     public int getDaysRented() {
         return daysRented;
