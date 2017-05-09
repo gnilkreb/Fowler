6ee620b664cc9dfbf1d90814f7cbd3f0fa242181
Rico <r.jambor@yahoo.de>
2017-05-09 12:25:44 +0200

    Add newlines to Movie class

--- src/Movie.java
+++ src/Movie.java
@@ -2,6 +2,7 @@ public class Movie {
     public static final int CHILDRENS = 2;
     public static final int REGULAR = 0;
     public static final int NEW_RELEASE = 1;
+
     private String title;
     private int priceCode;
 
@@ -9,6 +10,7 @@ public class Movie {
         this.title = title;
         this.priceCode = priceCode;
     }
+
     public int getPriceCode() {
         return priceCode;
     }
@@ -16,6 +18,7 @@ public class Movie {
     public void setPriceCode(int priceCode) {
         this.priceCode = priceCode;
     }
+
     public String getTitle (){
         return title;
     }
