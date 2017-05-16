61559ce62cd8a1aa7171c4cf8a7702fb66031b63
Rico <r.jambor@yahoo.de>
2017-05-16 10:24:11 +0200

    Refactor variable name in getCharge

--- src/Rental.java
+++ src/Rental.java
@@ -16,22 +16,22 @@ class Rental {
     }
 
     public double getCharge() {
-        double thisAmount = 0;
+        double price = 0;
         switch (this.getMovie().getPriceCode()) {
             case Movie.REGULAR:
-                thisAmount += 2;
+                price += 2;
                 if (this.getDaysRented() > 2)
-                    thisAmount += (this.getDaysRented() - 2) * 1.5;
+                    price += (this.getDaysRented() - 2) * 1.5;
                 break;
             case Movie.NEW_RELEASE:
-                thisAmount += this.getDaysRented() * 3;
+                price += this.getDaysRented() * 3;
                 break;
             case Movie.CHILDREN:
-                thisAmount += 1.5;
+                price += 1.5;
                 if (this.getDaysRented() > 3)
-                    thisAmount += (this.getDaysRented() - 3) * 1.5;
+                    price += (this.getDaysRented() - 3) * 1.5;
                 break;
         }
-        return thisAmount;
+        return price;
     }
 }
