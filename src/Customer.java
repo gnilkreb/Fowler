0c40dc072e0472445aba363a7eb4fce752299666
Rico <r.jambor@yahoo.de>
2017-05-22 10:28:49 +0200

    Extract code to dedicated method

--- src/Customer.java
+++ src/Customer.java
@@ -19,30 +19,29 @@ class Customer {
     }
 
     public String statement() {
-        double totalAmount = 0;
-        int frequentRenterPoints = 0;
-        Enumeration enum_rentals = rentals.elements();	    
         String result = "Rental Record for " + this.getName() + "\n";
         result += "\t" + "Title" + "\t" + "\t" + "Days" + "\t" + "Amount" + "\n";
 
-        while (enum_rentals.hasMoreElements()) {
-            double thisAmount = 0;
-            Rental rental = (Rental) enum_rentals.nextElement();
-            //determine amounts for each line
-            thisAmount = rental.getCharge();
-            // add frequent renter points
-            frequentRenterPoints ++;
-            // add bonus for a two day new release rental
-            if ((rental.getMovie().getPriceCode() == Movie.NEW_RELEASE) && rental.getDaysRented() > 1)
-                frequentRenterPoints ++;
-            //show figures for this rental
-            result += "\t" + rental.getMovie().getTitle()+ "\t" + "\t" + rental.getDaysRented() + "\t" + String.valueOf(thisAmount) + "\n";
-            totalAmount += thisAmount;
-        }
         //add footer lines
-        result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
+        result += "Amount owed is " + getTotalCharge() + "\n";
         result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
         return result;
+    }
+
+    private double getTotalCharge() {
+        int frequentRenterPoints = 0;
+        Enumeration enum_rentals = rentals.elements();
+        double totalAmount = 0;
+
+        while (enum_rentals.hasMoreElements()) {
+            double result = 0;
+            Enumeration rentals = this.rentals.elements();
+            while (rentals.hasMoreElements()) {
+                Rental each = (Rental) rentals.nextElement();
+                result += each.getCharge();
+            }
+            return result;
+        }
     }
 }
     
